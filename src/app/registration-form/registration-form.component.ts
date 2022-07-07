9072d203a2cfec26ff15e1d5ada011c154620778
Bikash Shah <bikash.shah@f1soft.com>
2022-07-07 13:11:57 +0545

    registration form

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserDetail } from '../model/user-detail';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.scss']
 })
 export class RegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  userInfoForm:FormGroup;
+  user!:UserDetail;
+  user1?:UserDetail;
+  check:boolean=false;
+  
+  name:string='';
+
+  constructor(
+    private formBuilder:FormBuilder
+  )
+   {
+    this.userInfoForm=this.formBuilder.group({
+      firstName:['',[Validators.required]],
+      lastName:['',[Validators.required]],
+      email:['',[Validators.required]],
+      address:['',[Validators.required]]
+    })
+    }
 
   ngOnInit(): void {
+  }
+
+  create():void{
+  //  console.log(firstName);
+  this.user1 = this.userInfoForm.value;
+  this.check=true;
   }
 
 }
--- src/app/registration-form/registration-form.module.ts
+++ src/app/registration-form/registration-form.module.ts
@@ -2,11 +2,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule,  NbButtonModule, NbSidebarComponent, NbSidebarModule, NbCardModule, NbInputModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule,  NbButtonModule, NbSidebarComponent, NbSidebarModule, NbCardModule, NbInputModule, NbTreeGridModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { RouterModule } from '@angular/router';
 import { RegistrationFormComponent } from './registration-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NbInputModule,
     FormsModule,
     ReactiveFormsModule,
-    NbCardModule
+    NbCardModule,
+    NbButtonModule,
+    NbTreeGridModule,
+    CommonModule
   ],
   exports:[
     RegistrationFormComponent
