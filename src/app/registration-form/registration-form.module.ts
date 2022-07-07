a7668f449bcf0866494ec7feee26a14be8a77235
Bikash Shah <bikash.shah@f1soft.com>
2022-07-07 10:29:34 +0545

    registration form created

--- src/app/registration-form/registration-form.module.ts
+++ src/app/registration-form/registration-form.module.ts
@@ -2,18 +2,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule,  NbButtonModule, NbSidebarComponent, NbSidebarModule, NbCardModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule,  NbButtonModule, NbSidebarComponent, NbSidebarModule, NbCardModule, NbInputModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { RouterModule } from '@angular/router';
 import { RegistrationFormComponent } from './registration-form.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     RegistrationFormComponent
   ],
   imports: [  
-    FormsModule
+    NbInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NbCardModule
   ],
   exports:[
     RegistrationFormComponent
